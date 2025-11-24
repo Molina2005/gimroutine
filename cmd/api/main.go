5f9924092175ba3dc4ea65df305a1b172787902a
Cristian Molina <cm1094871@gmail.com>
2025-11-24 13:54:33 -0500

    llamado funciones para creacion de usuarios

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,13 +4,23 @@ import (
 	"fmt"
 	"log"
 	"modulo/internal/database"
+	"modulo/internal/users"
+	"net/http"
 )
 
 func main() {
+	// Conexion a la base de datos
 	connect, err := database.ConnectDB()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer connect.Close()
 	fmt.Println("conexion exitosa a postgres")
+
+	// LLamado de las capas de users para poder crear usuarios
+	repo := users.NewRepository(connect)
+	service := users.NewService(repo)
+	handler := users.NewHandler(service)
+	http.HandleFunc("/users", handler.CreateUsers)
+	http.ListenAndServe(":8080", nil)
 }
