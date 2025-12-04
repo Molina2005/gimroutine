a7e0d5f94018c25025890e50f875c3b7428d3cf7
Cristian Molina <cm1094871@gmail.com>
2025-12-04 16:27:06 -0500

    logica para poder consultar informacion usuario

--- internal/users/users_consultInformation.go
+++ internal/users/users_consultInformation.go
@@ -1,14 +1,28 @@
 package users
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func (re *Repository) ConsultUserInformation(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, "Metodo no permitido", 404)
+	// recibe la peticion con el id a buscar y hace la conversion a int
+	IdParam := chi.URLParam(r, "id")
+	Id, err := strconv.Atoi(IdParam)
+	if err != nil {
+		http.Error(w, "Id invalido", 400)
+		return
 	}
-
-	// Seguir añadiendo logica de consulta
-
+	// Llamado consulta de repository
+	InfoUser, err := re.ViewUserInfomation(Id)
+	if err != nil {
+		// cualquier error de la consulta se va a reflejar aqui con err.Error()
+		http.Error(w, err.Error(), 404)
+		return
+	}
+	// Creacion de nuevo encoder json, para poder enviar informacion exacta al usuario
+	json.NewEncoder(w).Encode(InfoUser)
 }
