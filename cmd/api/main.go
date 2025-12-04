b3a4c29d8e25d90b81987f47acfe12590b73d3bd
Cristian Molina <cm1094871@gmail.com>
2025-12-04 16:24:03 -0500

    llamado funciones princiapales

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,8 @@ import (
 	"modulo/internal/database"
 	"modulo/internal/users"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
@@ -21,8 +23,16 @@ func main() {
 	repo := users.NewRepository(connect)
 	service := users.NewService(repo)
 	handler := users.NewHandler(service)
-	// llamado al handler de creacion de usuarios
-	http.HandleFunc("/users", handler.CreateUsers)
+
+	// Creacion de nuevo enrutador
+	r := chi.NewRouter()
+	// URL creacion de usuarios
+	r.Post("/users", handler.CreateUsers)
+	// URL consultar informaicion usuarios
+	// {id} : va enrutado con el id de chi.URLParam(r, "id")
+	r.Get("/users/{id}", repo.ConsultUserInformation)
+	// URL actualizar informacion usuario
+	r.Put("/users/{id}", handler.UpdateUsersInformation)
 	// Servidor escuchando en el puerto 8080
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", r)
 }
