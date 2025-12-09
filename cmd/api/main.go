2179e104c13672d5d19c7f3f1c17632bb95a9719
Cristian Molina <cm1094871@gmail.com>
2025-12-09 15:20:51 -0500

    rama principal

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	typeexercises "modulo/internal/TypeExercises"
 	"modulo/internal/database"
 	"modulo/internal/users"
 	"net/http"
@@ -23,15 +24,21 @@ func main() {
 	repo := users.NewRepository(connect)
 	service := users.NewService(repo)
 	handler := users.NewHandler(service)
+	// LLamado de las capas de users para poder crear tipos de ejercicios
+	repoTypeExercises := typeexercises.NewRepository(connect)
+	serviceTypeExercises := typeexercises.NewService(repoTypeExercises)
+	handlerTypeExercises := typeexercises.NewHandler(serviceTypeExercises)
 
 	// Creacion de nuevo enrutador
 	r := chi.NewRouter()
-	// URLS respuestas http
+	// URLS respuestas http (usuario)
 	// {id} : va enrutado con el id de chi.URLParam(r, "id")
 	r.Post("/users", handler.CreateUsers)
 	r.Get("/users/{id}", repo.ConsultUserInformation)
 	r.Put("/users/{id}", handler.UpdateUsersInformation)
 	r.Delete("/users/{id}", handler.DeleteUsers)
+	// URLS respuestas http (ejercicios)
+	r.Post("/TypeOfExercises", handlerTypeExercises.HandlerCreationTypeOfExercise)
 	// Servidor escuchando en el puerto 8080
 	http.ListenAndServe(":8080", r)
 }
