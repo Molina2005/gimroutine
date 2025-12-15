3994c01c57cd833a6a35bce0fd2aa9c32fd2ee6a
Cristian Molina <cm1094871@gmail.com>
2025-12-15 14:51:29 -0500

    logica interna del programa

---
+++ internal/Typeexercises/service.go
@@ -0,0 +1,26 @@
+package typeexercises
+
+import (
+	"errors"
+)
+
+type ServiceExercises struct {
+	repo *RepositoryExercises
+}
+
+func NewService(r *RepositoryExercises) *ServiceExercises {
+	return &ServiceExercises{repo: r}
+}
+
+// Requirimientos para la creacion de tipo de ejercicios
+func (s *ServiceExercises) ServiceCreationTypeOfExercise(nameTypeOfExercise, description string) error {
+	if nameTypeOfExercise == "" || description == "" {
+		return errors.New("todos los campos son obligatorios")
+	}
+	// Si no hay error ejecuta la consulta de Repository
+	return s.repo.QueryCreateExerciseType(nameTypeOfExercise, description)
+}
+
+func (s *ServiceExercises) ServiceUpdateInfoTypeOfExercise(IdTypeOfExercise int, nameTypeOfExercise, description string) error {
+	return s.repo.QueryUpdateTypeOfExercises(IdTypeOfExercise, nameTypeOfExercise, description)
+}
