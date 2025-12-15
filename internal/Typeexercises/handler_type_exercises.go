660bbdc2f879bc9dc14931bb08e3c2478d94d835
Cristian Molina <cm1094871@gmail.com>
2025-12-15 14:49:52 -0500

    actualizacion info de tipos de ejercicios

--- internal/Typeexercises/handler_type_exercises.go
+++
@@ -1,9 +0,0 @@
-package typeexercises
-
-type HandlerExercises struct {
-	service *ServiceExercises
-}
-
-func NewHandler(s *ServiceExercises) *HandlerExercises {
-	return &HandlerExercises{service: s}
-}
--- internal/Typeexercises/update_type_of_exercise.go
+++ internal/Typeexercises/update_type_of_exercise.go
@@ -1,1 +1,9 @@
 package typeexercises
+
+import "net/http"
+
+func (h *HandlerExercises) HandlerUpdateInfoTypeOfExercises(w http.ResponseWriter, r *http.Request) {
+
+	// mirar si se puede hacer sin el chi.param si no solo cuando se consulte se pueda actuzliar poiendo
+	// el meotod put
+}
