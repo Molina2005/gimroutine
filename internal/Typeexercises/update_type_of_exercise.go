62440e31c57232910aa48c613109fc4ad231fe21
Cristian Molina <cm1094871@gmail.com>
2025-12-16 15:05:42 -0500

    handler actualizacion infomracion tipos ejercicios

--- internal/Typeexercises/update_type_of_exercise.go
+++ internal/Typeexercises/update_type_of_exercise.go
@@ -1,9 +1,39 @@
 package typeexercises
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func (h *HandlerExercises) HandlerUpdateInfoTypeOfExercises(w http.ResponseWriter, r *http.Request) {
-
-	// mirar si se puede hacer sin el chi.param si no solo cuando se consulte se pueda actuzliar poiendo
-	// el meotod put
+	if r.Method != "PUT" {
+		http.Error(w, "Metodo no permitido", 404)
+		return
+	}
+	IdParam := chi.URLParam(r, "id")
+	Id, err := strconv.Atoi(IdParam)
+	if err != nil {
+		http.Error(w, "ID invalido", 400)
+		return
+	}
+	var inputUpdate struct {
+		NameTypeOfExercise string `json:"nameTypeOfExercise"`
+		Description        string `json:"description"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&inputUpdate); err != nil {
+		http.Error(w, "JSON invalido", 400)
+		return
+	}
+	if err := h.service.ServiceUpdateInfoTypeOfExercise(
+		Id,
+		inputUpdate.NameTypeOfExercise,
+		inputUpdate.Description,
+	); err != nil {
+		http.Error(w, err.Error(), 404)
+		return
+	}
+	w.Write([]byte("Tipo de ejercicio actualizado correctamente"))
 }
