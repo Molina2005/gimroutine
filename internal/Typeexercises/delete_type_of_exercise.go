a376f58790f4ba3df6eb6b5edb540e2f9bc497b3
Cristian Molina <cm1094871@gmail.com>
2025-12-16 15:04:41 -0500

    handler para eliminacion tipos de ejercicios

--- internal/Typeexercises/delete_type_of_exercise.go
+++ internal/Typeexercises/delete_type_of_exercise.go
@@ -1,1 +1,26 @@
 package typeexercises
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func (h *HandlerExercises) DeleteTypeOfExercises(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "DELETE" {
+		http.Error(w, "Metodo no permitido", 404)
+		return
+	}
+	IdParam := chi.URLParam(r, "id")
+	id, err := strconv.Atoi(IdParam)
+	if err != nil {
+		http.Error(w, "Id invalido", 400)
+		return
+	}
+	if err := h.service.ServiceDeleteTypeOfExercise(id); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	w.Write([]byte("tipo ejercicio eliminado con exito"))
+}
