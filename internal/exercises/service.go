7d9ca650a5ac2e6ef485781a1f37472a637d30d3
Cristian Molina <cm1094871@gmail.com>
2025-12-19 14:18:22 -0500

    logica principal de ejercicios

--- internal/exercises/service.go
+++ internal/exercises/service.go
@@ -1,9 +1,18 @@
 package exercises
 
+import "errors"
+
 type ServiceExercises struct {
 	Repo *RepositoryExercises
 }
 
 func NewService(r *RepositoryExercises) *ServiceExercises {
 	return &ServiceExercises{Repo: r}
+}
+
+func (s *ServiceExercises) ServiceCreationExercises(IdTypeOfExercise int, nameTypeOfExercise, description, image string) error {
+	if IdTypeOfExercise <= 0 || nameTypeOfExercise == "" || description == "" || image == "" {
+		return errors.New("todos los campos son obligatorios")
+	}
+	return s.Repo.QueryCreateExercises(IdTypeOfExercise, nameTypeOfExercise, description, image)
 }
