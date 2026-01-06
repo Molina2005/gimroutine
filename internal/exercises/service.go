085671b52b8b425344876b6072631df8813a029d
Cristian Molina <cm1094871@gmail.com>
2026-01-06 14:20:58 -0500

    servicios con logica interna de cada handler

--- internal/exercises/service.go
+++ internal/exercises/service.go
@@ -10,9 +10,24 @@ func NewService(r *RepositoryExercises) *ServiceExercises {
 	return &ServiceExercises{Repo: r}
 }
 
+// Servicio creacion de ejercicios
 func (s *ServiceExercises) ServiceCreationExercises(IdTypeOfExercise int, nameTypeOfExercise, description, image string) error {
 	if IdTypeOfExercise <= 0 || nameTypeOfExercise == "" || description == "" || image == "" {
 		return errors.New("todos los campos son obligatorios")
 	}
-	return s.Repo.QueryCreateExercises(IdTypeOfExercise, nameTypeOfExercise, description, image)
+	return s.Repo.QueryCreateExercises(
+		IdTypeOfExercise,
+		nameTypeOfExercise,
+		description,
+		image)
+}
+
+// Servicio actualizacion de ejercicios
+func (s *ServiceExercises) ServiceUpdateExercises(IdExercise, IdTypeOfExercise int, nameTypeOfExercise, description, image string) error {
+	return s.Repo.QueryUpdateExercise(
+		IdExercise,
+		IdTypeOfExercise,
+		nameTypeOfExercise,
+		description,
+		image)
 }
