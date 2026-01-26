40e3c4a1860301011846d26d917c22114c5d2aba
Cristian Molina <cm1094871@gmail.com>
2026-01-26 09:44:02 -0500

    logica principal de cada handler

--- internal/users/service.go
+++ internal/users/service.go
@@ -17,26 +17,26 @@ func NewService(r *RepositoryUsers) *ServiceUsers {
 }
 
 // Creacion de usuario y requirimientos a seguir
-func (r *ServiceUsers) CreatetUser(name, email string, age int, weight int16, height float64, password string) error {
+func (r *ServiceUsers) CreatetUser(name, email string, password string) error {
 	// Contraseña incriptada
 	HashPassword, err := utils.HashPassword(password)
 	if err != nil {
 		fmt.Println("error al hashear contraseña")
 	}
 	// Validaion de se pemiten campos vacios
-	if name == "" || email == "" || age <= 0 || weight <= 0 || height <= 0 || HashPassword == "" {
+	if name == "" || email == "" || HashPassword == "" {
 		return errors.New("todos los campos son obligatorios")
 	}
-	return r.repo.InsertUser(name, email, age, weight, height, HashPassword)
+	return r.repo.InsertUser(name, email, HashPassword)
 }
 
 // Requerimientos actualizacion informacion usuario
-func (r *ServiceUsers) UpdateUserInformation(id_usuarios int, name, email string, age int, weight int16, height float64, password string) error {
+func (r *ServiceUsers) UpdateUserInformation(id_usuarios int, name, email string, password string) error {
 	HashPassword, err := utils.HashPassword(password)
 	if err != nil {
 		fmt.Println("error al hashear contraseña")
 	}
-	return r.repo.UpdateUser(id_usuarios, name, email, age, weight, height, HashPassword)
+	return r.repo.UpdateUser(id_usuarios, name, email, HashPassword)
 }
 
 // Requerimientos eliminacion usuario
