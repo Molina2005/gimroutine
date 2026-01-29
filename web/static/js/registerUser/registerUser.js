74a579ed61ae5b190cf3efd51ba3f59c576cf4d8
Cristian Molina <cm1094871@gmail.com>
2026-01-28 20:41:50 -0500

    logica registro de usuarios

--- web/static/js/registerUser/registerUser.js
+++ web/static/js/registerUser/registerUser.js
@@ -1,10 +1,10 @@
 // Importa la función createUser desde api.js,
 // que se encarga de comunicarse con el backend
-import { createUser } from "./api.js";
+import { connectionUser } from "./api.js";
 // Función que maneja la lógica de registro de usuarios.
 // Recibe los datos del formulario y delega la creación al backend.
-export function registerUser(formData){
-    return createUser(formData)
+export function logicRegisterUser(formData){
+    return connectionUser(formData)
     .then(data =>{
         console.log("usuario creado")
         return data
