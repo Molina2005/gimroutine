b460389a98ecf50abf5aabe8fda3668c96b67515
Cristian Molina <cm1094871@gmail.com>
2026-01-28 20:36:11 -0500

    serivice con logica principal de funciones

--- internal/users/service.go
+++ internal/users/service.go
@@ -3,6 +3,7 @@ package users
 import (
 	"errors"
 	"fmt"
+	"modulo/internal/models"
 	"modulo/internal/utils"
 )
 
@@ -17,7 +18,7 @@ func NewService(r *RepositoryUsers) *ServiceUsers {
 }
 
 // Creacion de usuario y requirimientos a seguir
-func (r *ServiceUsers) CreatetUser(name, email string, password string) error {
+func (r *ServiceUsers) ServiceCreatetUser(name, email string, password string) error {
 	// Contraseña incriptada
 	HashPassword, err := utils.HashPassword(password)
 	if err != nil {
@@ -27,19 +28,27 @@ func (r *ServiceUsers) CreatetUser(name, email string, password string) error {
 	if name == "" || email == "" || HashPassword == "" {
 		return errors.New("todos los campos son obligatorios")
 	}
-	return r.repo.InsertUser(name, email, HashPassword)
+	return r.repo.QueryInsertUser(name, email, HashPassword)
 }
 
 // Requerimientos actualizacion informacion usuario
-func (r *ServiceUsers) UpdateUserInformation(id_usuarios int, name, email string, password string) error {
+func (r *ServiceUsers) ServiceUpdateUserInformation(id_usuarios int, name, email string, password string) error {
 	HashPassword, err := utils.HashPassword(password)
 	if err != nil {
 		fmt.Println("error al hashear contraseña")
 	}
-	return r.repo.UpdateUser(id_usuarios, name, email, HashPassword)
+	return r.repo.QueryUpdateUser(id_usuarios, name, email, HashPassword)
 }
 
 // Requerimientos eliminacion usuario
-func (r *ServiceUsers) DeleteUser(id_usuarios int) error {
-	return r.repo.DeleteUser(id_usuarios)
+func (r *ServiceUsers) ServiceDeleteUser(id_usuarios int) error {
+	return r.repo.QueryDeleteUser(id_usuarios)
+}
+
+// Requerimientos login
+func (r *ServiceUsers) ServiceLogin(email, password string) (*models.Login, error) {
+	if email == "" || password == "" {
+		return nil, errors.New("email y contraseña son obligatorios")
+	}
+	return r.repo.QueryLogin(email, password)
 }
