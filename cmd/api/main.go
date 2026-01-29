31cc6407b2234a8c222675fceb32ad49485a87f0
Cristian Molina <cm1094871@gmail.com>
2026-01-28 20:32:42 -0500

    rama principal

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -36,15 +36,16 @@ func main() {
 	r := chi.NewRouter()
 	// URLS respuestas http (usuario)
 	// {id} : va enrutado con el id de chi.URLParam(r, "id")
-	r.Post("/users", handler.CreateUsers)
-	r.Get("/users/{id}", repo.ConsultUserInformation)
-	r.Put("/users/{id}", handler.UpdateUsersInformation)
-	r.Delete("/users/{id}", handler.DeleteUsers)
+	r.Post("/users", handler.HandlerCreateUsers)
+	r.Post("/login", handler.HandlerLogin)
+	r.Get("/users/{id}", repo.HandlerConsultUserInformation)
+	r.Put("/users/{id}", handler.HandlerUpdateUsersInformation)
+	r.Delete("/users/{id}", handler.HandlerDeleteUsers)
 	// URLS respuestas http (tipos de ejercicios)
 	r.Post("/TypeOfExercises", handlerTypeExercises.HandlerCreationTypeOfExercise)
 	r.Get("/TypeOfExercises/{id}", repoTypeExercises.HandlerConsultTypeOfExercises)
 	r.Put("/TypeOfExercises/{id}", handlerTypeExercises.HandlerUpdateInfoTypeOfExercises)
-	r.Delete("/TypeOfExercises/{id}", handlerTypeExercises.DeleteTypeOfExercises)
+	r.Delete("/TypeOfExercises/{id}", handlerTypeExercises.HandlerDeleteTypeOfExercises)
 	// URLS respuestas http (ejercicios)
 	r.Post("/Exercises", handlerExercises.HandlerCreationExercises)
 	r.Get("/Exercises/{id}", repoExercises.HandlerConsultInformationExercise)
@@ -55,11 +56,20 @@ func main() {
 		http.ServeFile(w, r, "./web/templates/index.html")
 	})
 	// Permite establecer la ruta html de crear usuario
-	r.Get("/createaccount", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./web/templates/create_account.html")
+	r.Get("/createAccount", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/templates/users/create_account.html")
 	})
+	// Permite establecer la ruta html del login
+	r.Get("/loginUsers", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/templates/users/login.html")
+	})
+	// Pagina inicial al iniciar sesion
+	r.Get("/Home", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/templates/pages/home.html")
+	})
+
 	// Ruta para poder trabajar con los archivos statics como css, js, etc...
 	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./web/static/"))))
 	// Servidor escuchando en el puerto 8080
-	http.ListenAndServe(":4545", r)
+	http.ListenAndServe(":8080", r)
 }
