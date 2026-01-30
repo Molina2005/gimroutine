2010c86e80c48586377fb06b018e6286bf6b37fe
Cristian Molina <cm1094871@gmail.com>
2026-01-29 19:52:10 -0500

    funcionalidad de pagina con el dom

--- web/static/js/loginUser/mainLogin.js
+++ web/static/js/loginUser/mainLogin.js
@@ -1,21 +1,22 @@
 import { logicLogin } from "./Login.js";
 document.addEventListener("DOMContentLoaded", function(){
-    console.log("entro a login de inicio de sesion")
     const form =  document.getElementById("loginUserForm");
-    form.addEventListener("submit", function(e){
+    form.addEventListener("submit", async function(e){
         e.preventDefault();
         const formData = {
             name: document.getElementById("nameOrEmail").value,
             password: document.getElementById("password").value
-        }
-        logicLogin(formData);
-        
-    })
-    .then(data =>{
-        form.reset();
-        window.location.replace("/Home")
-    })
-    .catch(err =>{
-        alert("Credenciales incorrectas, Intente de nuevo")
+        };
+        // Try maneja el error sin rompero el programa 
+        // catch cumple con enviar el error en caso de que falle
+        try{
+            // Await se usa para esperar una promesa, hasta que esto se cumpla pasa a lo siguiente
+            await logicLogin(formData);
+            form.reset();
+            // url para que redirija a la pagina de Home
+            window.location.replace("/Home");
+        }catch (err){
+            alert("Credenciales incorrectas intente de nuevo")
+        } 
     })
 })
