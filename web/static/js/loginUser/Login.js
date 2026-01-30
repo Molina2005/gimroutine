d60ae42771cd4edb68d45008e2935f2863463208
Cristian Molina <cm1094871@gmail.com>
2026-01-29 19:51:13 -0500

    logica principal del login

--- web/static/js/loginUser/Login.js
+++ web/static/js/loginUser/Login.js
@@ -1,6 +1,5 @@
 import { login } from "./api.js"
 export function logicLogin(formData){
-    console.log("entro a logica login")
     return login(formData)
     .then(data =>{
         console.log("Ingreso al sistema")
