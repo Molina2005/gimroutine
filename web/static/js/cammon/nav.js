e4e296b897260a5b17cde4551f0ba08f035e99e8
Cristian Molina <cm1094871@gmail.com>
2026-02-24 21:03:34 -0500

    funciones comunes js las cuales son reutilizables

---
+++ web/static/js/cammon/nav.js
@@ -0,0 +1,15 @@
+// funcion para cargar menus reutilizables en otro archivo html
+function cargarMenu(nombreArchivo, idContenedor = 'container-nav'){
+    // archivo el cual contiene la data  
+    fetch(nombreArchivo) 
+    // Promesa que convierte data que llega en texto para poder utilizarlo
+    // Tiene que cumplirse el primer then para poder avanzar al siguiente
+    .then(res => res.text())
+    // resultado de la promesa anterior
+    .then(data =>{
+        // Se intercambia lo que esta dentro de idContenedor (lo que esta dentro de container-nav) por lo que esta dentro de data(lo que esta dentro del archivo cargado)
+        document.getElementById(idContenedor).innerHTML = data
+    });
+}
+
+
