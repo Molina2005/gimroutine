cf6c08b6ca22f3d949c2c4a317298c314daed48a
Cristian Molina <cm1094871@gmail.com>
2026-02-28 17:16:51 -0500

    handler actualizacion de tipos de ejercicios en formato json

--- internal/Typeexercises/update_type_of_exercise.go
+++ internal/Typeexercises/update_type_of_exercise.go
@@ -8,15 +8,21 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func (h *HandlerExercises) HandlerUpdateInfoTypeOfExercises(w http.ResponseWriter, r *http.Request) {
+func (h *HandlerTypeOfExercises) HandlerUpdateInfoTypeOfExercises(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPut {
-		http.Error(w, "Metodo no permitido", 404)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "metodo_no_permitido",
+		})
 		return
 	}
 	IdParam := chi.URLParam(r, "id")
 	Id, err := strconv.Atoi(IdParam)
 	if err != nil {
-		http.Error(w, "ID invalido", 400)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "id_invalido",
+		})
 		return
 	}
 	var inputUpdate struct {
@@ -24,7 +30,10 @@ func (h *HandlerExercises) HandlerUpdateInfoTypeOfExercises(w http.ResponseWrite
 		Description        string `json:"description"`
 	}
 	if err := json.NewDecoder(r.Body).Decode(&inputUpdate); err != nil {
-		http.Error(w, "JSON invalido", 400)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "json_invalido",
+		})
 		return
 	}
 	if err := h.service.ServiceUpdateInfoTypeOfExercise(
@@ -32,8 +41,13 @@ func (h *HandlerExercises) HandlerUpdateInfoTypeOfExercises(w http.ResponseWrite
 		inputUpdate.NameTypeOfExercise,
 		inputUpdate.Description,
 	); err != nil {
-		http.Error(w, err.Error(), 404)
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": err.Error(),
+		})
 		return
 	}
-	w.Write([]byte("Tipo de ejercicio actualizado correctamente"))
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "tipo_ejercicio_actualizado_correctamente",
+	})
 }
