56aa445c6c29a89df8a8f3ee82812d7f2da8e9a7
Cristian Molina <cm1094871@gmail.com>
2026-02-28 17:21:02 -0500

    handler login usuario en formato json

--- internal/users/login.go
+++ internal/users/login.go
@@ -6,8 +6,12 @@ import (
 )
 
 func (h *HandlerUsers) HandlerLogin(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	if r.Method != http.MethodPost {
-		http.Error(w, "Metodo no permitido", http.StatusMethodNotAllowed)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "metodo_no_permitido",
+		})
 		return
 	}
 	var input struct {
@@ -16,9 +20,12 @@ func (h *HandlerUsers) HandlerLogin(w http.ResponseWriter, r *http.Request) {
 	}
 	// Decodifica el JSON recibido en el body de la petición
 	// y lo convierte en la estructura input.
-	// Si el JSON es inválido, responde con 400 Bad Request.
+	// Si el JSON es inválido, responde con  StatusBadRequest.
 	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-		http.Error(w, "JSON invalido", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "json_invalido",
+		})
 		return
 	}
 	// Se llama al servicio para validar las credenciales.
@@ -26,10 +33,13 @@ func (h *HandlerUsers) HandlerLogin(w http.ResponseWriter, r *http.Request) {
 	if _, err := h.service.ServiceLogin(input.Email, input.Password); err != nil {
 		// WriteHeader: Guarda el codigo de estado de la respuesta
 		w.WriteHeader(http.StatusUnauthorized)
-		json.NewEncoder(w).Encode(map[string]string{"error": "Credenciales invalidas"})
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "Credenciales invalidas",
+		})
 		return
 	}
 	// Envio a frontend login exitoso
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"message": "Login exitoso"})
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "Login exitoso",
+	})
 }
