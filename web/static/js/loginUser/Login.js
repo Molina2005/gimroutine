74ca38d69ba4c83c5edf3920e395ecce8518617b
Cristian Molina <cm1094871@gmail.com>
2026-03-03 23:40:54 -0500

    logica web la cual maneja todo lo relacionado con la informacion que recibe del fetch

--- web/static/js/loginUser/Login.js
+++ web/static/js/loginUser/Login.js
@@ -1,8 +1,5 @@
 import { login } from "./api.js"
 export async function logicLogin(formData){
     const data = await login(formData);
-    if (data.error){
-        throw new Error(data.message || "Credenciales invalidas");
-    }
     return data;
 }
