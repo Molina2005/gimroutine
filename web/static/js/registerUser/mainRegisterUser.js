7294a5c0c2836b3c1cf88088c3f60da94d893d27
Cristian Molina <cm1094871@gmail.com>
2026-03-03 23:42:47 -0500

    dom el cual maneja toda interaccion del usuario

--- web/static/js/registerUser/mainRegisterUser.js
+++ web/static/js/registerUser/mainRegisterUser.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(){
     // Varible que obtiene informacion del formulario 
     const form = document.getElementById("userForm")
     // Evento que al momento de dar oprimir el boton hace todo lo que se contiene dentro de la funcion
-    form.addEventListener("submit", function(e){
+    form.addEventListener("submit", async function(e){
         // evita el comportamiento por defecto del formulario (recargar la página) y perder la informacion.
         e.preventDefault();
         // FormData va a guardar toda la informacion que se obtega dentro de esa const
@@ -14,10 +14,21 @@ document.addEventListener("DOMContentLoaded", function(){
             email: document.getElementById("email").value,
             password: document.getElementById("password").value,
         }
-        // Se le pasa a la función login la información del formData
-        // para que ejecute la lógica de inicio de sesión (comunicación con el backend)
-        logicRegisterUser(formData)
-        form.reset();
-    })
-})
+        try{
+            // Se le pasa a la función login la información del formData
+            // para que ejecute la lógica de inicio de sesión (comunicación con el backend)
+            const data = await logicRegisterUser(formData);
+            alert("Usuario creado correctamente",data);
+            form.reset();
+            // Envio al login en el momento en que se cree el usuario
+            window.location.href = "/loginUsers"
+        }catch (err){
+            if (err.status == "409"){
+                alert("Usuario ya existe en el sistema")
+            }else{
+                alert("Error en el servidor")
+            }
+        }
+    });
+});
 
