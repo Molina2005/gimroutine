3bfa3b164d44cb778158f4f867c5a88605c4cee1
Cristian Molina <cm1094871@gmail.com>
2026-03-20 12:48:28 -0500

    eliminacion archivos exercises_users

--- internal/exercises_users/service.go
+++
@@ -1,9 +0,0 @@
-package exercisesusers
-
-type ServiceExercisesUsers struct {
-	repo *RepositoryExercisesUsers
-}
-
-func NewService(r *RepositoryExercisesUsers) *ServiceExercisesUsers {
-	return &ServiceExercisesUsers{repo: r}
-}
