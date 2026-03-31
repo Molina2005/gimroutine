7ac3f95c6abb36c3236ba3342d08eb37d26ab070
Cristian Molina <cm1094871@gmail.com>
2026-03-30 21:14:59 -0500

    handler creacion de clientes por peticion

--- internal/clients/creation_client.go
+++ internal/clients/creation_client.go
@@ -1,7 +1,46 @@
 package clients
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
 
-func (h *HandlerClients) HandlerCreationClients(w http.ResponseWriter, r http.Request) {
-
+func (h *HandlerClients) HandlerCreationClients(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method != http.MethodPost {
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "metodo_no_permitido",
+		})
+		return
+	}
+	var input struct {
+		Name     string `json:"name"`
+		Document string `json:"document"`
+		Gmail    string `json:"gmail"`
+		Phone    string `json:"phone"`
+		Password string `json:"password"`
+		State    string `json:"state"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "json_invalido",
+		})
+		return
+	}
+	if err := h.service.ServiceCreateClient(input.Name, input.Document, input.Gmail, input.Phone, input.Password, input.State); err != nil {
+		if errors.Is(err, ErrClientAlreadyExists) {
+			w.WriteHeader(http.StatusConflict)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "cliente_creado_correctamente",
+	})
 }
