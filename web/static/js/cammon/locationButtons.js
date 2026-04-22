3282f82e780bda5a29ed8b596c1d5540b72d9d31
Cristian Molina <cm1094871@gmail.com>
2026-04-21 21:20:48 -0500

    funcion para identificar cliente y manejar boton dependiendo client

---
+++ web/static/js/cammon/locationButtons.js
@@ -0,0 +1,9 @@
+export async function LocationButtons(btnClass, identificator){
+    const buttons = document.querySelectorAll(`.${btnClass}`);
+    buttons.forEach(btn =>{
+        btn.addEventListener("click", ()=>{
+            
+        })
+    })
+
+}
