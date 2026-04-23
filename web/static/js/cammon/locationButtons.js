dcb17a396e2cc229952c61d109cbb7380883b586
Cristian Molina <cm1094871@gmail.com>
2026-04-22 22:02:25 -0500

    funcion para poder darle funcionamiento a botones segun cliente

--- web/static/js/cammon/locationButtons.js
+++ web/static/js/cammon/locationButtons.js
@@ -1,9 +1,14 @@
-export async function LocationButtons(btnClass, identificator){
+// Funcion para la redireccion de cada boton de acciones de clientes 
+// Recibe como parametro, clase del boton y identificador del cliente en este caso document
+export async function LocationButtons(btnClass, clientData){
+    // Se obtiene cada uno de los botones 
     const buttons = document.querySelectorAll(`.${btnClass}`);
+    // se recorren cada uno de los botones y se guardan en btn
     buttons.forEach(btn =>{
+        // Se crea evento que al dar click obtiene el id(documento) de ese cliente y luego lo pasa al parametro clientData
         btn.addEventListener("click", ()=>{
-            
+            const clientId = btn.dataset.id
+            clientData(clientId)
         })
     })
-
 }
