d77802ba9b827236425f558f15f7601bc7015609
Cristian Molina <cm1094871@gmail.com>
2026-07-07 20:44:02 -0500

    conexion a la base de datos

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -6,24 +6,33 @@ import (
 	"log"
 	"os"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 )
 
 func ConnectDB() (*pgxpool.Pool, error) {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("error al cargar archivo .env", err)
-	}
+	// cargue de archivo .env
+	_ = godotenv.Load()
+
 	connectionChain := fmt.Sprintf(
-		"user=%v password=%v dbname=%v host=%v port=%v sslmode=disable",
+		// Formato que pide supabase para la conexion
+		"postgres://%s:%s@%s:%s/%s?sslmode=require",
 		os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("POSTGRES_DB"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
+		os.Getenv("POSTGRES_DB"),
 	)
-	pool, err := pgxpool.New(context.Background(), connectionChain)
+	// Se desactiva configuracion de protocolo avanzado que trae pgxpool para las consultas
+	// Y se pasa al protocola simple el cual solo envia la consulta y ya
+	config, err := pgxpool.ParseConfig(connectionChain)
+	if err != nil {
+		log.Fatal("error al obtener config", err)
+	}
+	// Se pasa al nuevo protocola simple
+	config.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeSimpleProtocol
+	pool, err := pgxpool.NewWithConfig(context.Background(), config)
 	if err != nil {
 		log.Fatal("error conectando a la base de datos", err)
 	}
