22c4f1fe0f29c8d55902a715adb7d7e2d992ccab
Cristian Molina <cm1094871@gmail.com>
2026-07-07 20:51:27 -0500

    funcion unica para poder obtener botones de update, delete, consult y asi redireccione al campo exacto

--- web/static/js/cammon/locationButtons.js
+++ web/static/js/cammon/locationButtons.js
@@ -1,11 +1,11 @@
 // Funcion para la redireccion de cada boton de acciones de clientes 
-// Recibe como parametro, clase del boton y identificador del cliente en este caso document
+// Recibe como parametro, clase del boton y identificador del cliente en este caso id
 export async function LocationButtons(btnClass, clientData){
     // Se obtiene cada uno de los botones 
     const buttons = document.querySelectorAll(`.${btnClass}`);
     // se recorren cada uno de los botones y se guardan en btn
     buttons.forEach(btn =>{
-        // Se crea evento que al dar click obtiene el id(documento) de ese cliente y luego lo pasa al parametro clientData
+        // Se crea evento que al dar click obtiene el id de ese cliente y luego lo pasa al parametro clientData
         btn.addEventListener("click", ()=>{
             const clientId = btn.dataset.id
             clientData(clientId)
