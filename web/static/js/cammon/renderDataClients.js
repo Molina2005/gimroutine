1d5e8bd590c0396d6532f9d6f5de8d763e91acc9
Cristian Molina <cm1094871@gmail.com>
2026-07-20 18:49:27 -0500

    funciones que devuelve filas y columnas y botones para poder cargarle informacion de clientes

---
+++ web/static/js/cammon/renderDataClients.js
@@ -0,0 +1,24 @@
+import { RenderCrudButton } from "./locationButtons.js";
+export function RenderClientsRow(client){
+    const row = document.createElement("tr");
+    const cellName = document.createElement("td");
+    cellName.textContent = client.Name;
+    row.appendChild(cellName);
+    const cellDocument = document.createElement("td");
+    cellDocument.textContent = client.Document;
+    row.appendChild(cellDocument);
+    const cellEmail = document.createElement("td");
+    cellEmail.textContent = client.Gmail;
+    row.appendChild(cellEmail);
+    const cellPhone = document.createElement("td");
+    cellPhone.textContent = client.Phone;
+    row.appendChild(cellPhone);
+    const cellEntryDate = document.createElement("td");
+    cellEntryDate.textContent = client.EntryDate;
+    row.appendChild(cellEntryDate);
+    const cellState = document.createElement("td");
+    cellState.textContent = client.State;
+    row.appendChild(cellState);
+    row.appendChild(RenderCrudButton(client.Id))
+    return row
+}
