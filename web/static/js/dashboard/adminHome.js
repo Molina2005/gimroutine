4061423e75a50b7e7de2f02b5d4332314b44ec70
Cristian Molina <cm1094871@gmail.com>
2026-08-01 20:09:54 -0500

    panel de inicio del administrador

--- web/static/js/dashboard/adminHome.js
+++ web/static/js/dashboard/adminHome.js
@@ -1,5 +1,5 @@
-import { VerifyToken } from "../users/loginUser/tokenVerification.js";
-import { Logout } from "../users/loginUser/tokenLogout.js";
+import { VerifyToken } from "../login/tokenVerification.js";
+import { Logout } from "../login/tokenLogout.js";
 
 // DOM para poder manejar que al momento que el usaurio de click en cerrar sesion el token se elimine y no lo deje ingresar
 document.addEventListener("DOMContentLoaded",()=>{
