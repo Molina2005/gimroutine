25a76dd5f8b069504f7fdc7649306aef92f15207
Cristian Molina <cm1094871@gmail.com>
2026-08-01 19:55:00 -0500

    funciones para poder reutilizar codigo

--- internal/utils/convert.go
+++ internal/utils/convert.go
@@ -1,9 +1,17 @@
 package utils
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"golang.org/x/crypto/bcrypt"
+)
 
 // Hashear contraseña
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(bytes), err
+}
+
+// Funcion apra comparar hash de base de datos con contraseña que el usuario ingresa
+func ComparePassword(hash, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+
 }
