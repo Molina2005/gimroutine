ec823d94fca7c01d9d7eaa7ff0405926bf0e50c7
Cristian Molina <cm1094871@gmail.com>
2026-08-01 19:50:43 -0500

    consultas a la base de datos

--- internal/clients/repository.go
+++ internal/clients/repository.go
@@ -27,18 +27,6 @@ func (r *RepositoryClients) QueryClientExistsById(IdClient int) (bool, error) {
 	return ExistsId, nil
 }
 
-// Crear funcion validar existencia por correo
-func (r *RepositoryClients) QueryClientExistsByGmail(Gmail string) (bool, error) {
-	ctx := context.Background()
-	var ExistsGmail bool
-	query := `SELECT EXISTS(SELECT 1 FROM clientes WHERE correo = $1)`
-	err := r.db.QueryRow(ctx, query, Gmail).Scan(&ExistsGmail)
-	if err != nil {
-		return false, err
-	}
-	return ExistsGmail, nil
-}
-
 // Crear fncion valdar exixtencia por documento
 func (r *RepositoryClients) QueryClientExistsByDocument(Document string) (bool, error) {
 	ctx := context.Background()
@@ -63,21 +51,42 @@ func (r *RepositoryClients) QueryCreateClient(name, document, gmail, phone, pass
 	return nil
 }
 
+// Consultar Informacion cliente
+func (r *RepositoryClients) QueryViewClientInfomation(id_client int) (*models.Client, error) {
+	ctx := context.Background()
+	// Guarda la informacion que se envia con Scan
+	var DataClient models.Client
+	query := "SELECT nombre, documento, correo, telefono, contrasena, estado FROM clientes WHERE id_cliente=$1"
+	// Envia informacion con scan a la variable DataClient
+	if err := r.db.QueryRow(ctx, query, id_client).Scan(
+		&DataClient.Name,
+		&DataClient.Document,
+		&DataClient.Gmail,
+		&DataClient.Phone,
+		&DataClient.Password,
+		&DataClient.State,
+	); err != nil {
+		return nil, err
+	}
+	// retorna la informacion o un error
+	return &DataClient, nil
+}
+
 // Consultar informacion de clientes
-func (r *RepositoryClients) QueryClientInformation() ([]models.Client, error) {
+func (r *RepositoryClients) QueryClientInformation() ([]models.Clients, error) {
 	ctx := context.Background()
 	query := `SELECT id_cliente, nombre, documento, correo, telefono, fecha_ingreso, 
-		contrasena, estado FROM clientes`
+		contrasena, estado FROM clientes ORDER BY id_cliente ASC`
 	// Trae todos los registros
 	data, err := r.db.Query(ctx, query)
 	if err != nil {
 		return nil, err
 	}
 	defer data.Close()
-	var dataClients []models.Client
+	var dataClients []models.Clients
 	// Recorre cada registro uno por uno
 	for data.Next() {
-		var client models.Client
+		var client models.Clients
 		// Se agregan a memoria a client para luego pasarlo a dataclient
 		if err := data.Scan(&client.Id, &client.Name, &client.Document,
 			&client.Gmail, &client.Phone, &client.EntryDate,
@@ -92,7 +101,7 @@ func (r *RepositoryClients) QueryClientInformation() ([]models.Client, error) {
 // Actualizacion informacion cliente con contraseña incluida
 func (r *RepositoryClients) QueryUpdateClient(id_client int, name, document, gmail, phone, password, state string) error {
 	ctx := context.Background()
-	query := `UPDATE clientes SET nombre = $1 documento = $2 correo = $3, telefono = $4, contrasena = $5, estado = $6 
+	query := `UPDATE clientes SET nombre = $1, documento = $2, correo = $3, telefono = $4, contrasena = $5, estado = $6 
 	WHERE id_cliente = $7`
 	if _, err := r.db.Exec(ctx, query, name, document, gmail, phone, password, state, id_client); err != nil {
 		return err
@@ -103,10 +112,46 @@ func (r *RepositoryClients) QueryUpdateClient(id_client int, name, document, gma
 // Actualizacion informacion cliente sin contraseña incluida
 func (r *RepositoryClients) QueryUpdateClientNoPassword(id_client int, name, document, gmail, phone, state string) error {
 	ctx := context.Background()
-	query := `UPDATE clientes SET nombre = $1 documento = $2 correo = $3, telefono = $4, esatdo = $5
+	query := `UPDATE clientes SET nombre = $1, documento = $2, correo = $3, telefono = $4, estado = $5
 	WHERE id_cliente = $6`
 	if _, err := r.db.Exec(ctx, query, name, document, gmail, phone, state, id_client); err != nil {
 		return err
 	}
 	return nil
+}
+
+// Eliminar cliente del sistema
+func (r *RepositoryClients) QueryDeleteClient(id_client int) error {
+	ctx := context.Background()
+	query := `DELETE FROM clientes WHERE id_cliente = $1`
+	_, err := r.db.Exec(ctx, query, id_client)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Buscar cliente en buscador
+func (r *RepositoryClients) QuerySearchClients(search string) ([]models.SearchClients, error) {
+	ctx := context.Background()
+	query := `SELECT nombre, documento, correo, telefono, contrasena, fecha_ingreso, estado 
+	FROM clientes 
+	WHERE nombre ILIKE '%' || $1 || '%' 
+	OR documento ILIKE '%' || $1 || '%' 
+	OR correo ILIKE '%' || $1 || '%'`
+	data, err := r.db.Query(ctx, query, search)
+	if err != nil {
+		return nil, err
+	}
+	var DataClients []models.SearchClients
+	defer data.Close()
+	for data.Next() {
+		var User models.SearchClients
+		err := data.Scan(&User.Name, &User.Document, &User.Gmail, &User.Phone, &User.Password, &User.EntryDate, &User.State)
+		if err != nil {
+			return nil, err
+		}
+		DataClients = append(DataClients, User)
+	}
+	return DataClients, nil
 }
