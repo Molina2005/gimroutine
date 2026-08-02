b77093963e06a24f6f2232073a402223768ceb61
Cristian Molina <cm1094871@gmail.com>
2026-08-01 19:49:10 -0500

    api

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
-	teammaintenance "modulo/internal/TeamMaintenance"
 	"modulo/internal/clients"
 	"modulo/internal/database"
-	maintenances "modulo/internal/maintenance"
+	"modulo/internal/login"
 	"modulo/internal/users"
 	"net/http"
 	"os"
@@ -22,46 +21,38 @@ func main() {
 	}
 	defer connect.Close()
 	fmt.Println("conexion exitosa a postgres")
+	// Llamado capas para poder manipular LOGIN
+	repoLogin := login.NewRepository(connect)
+	serviceLogin := login.NewService(repoLogin)
+	handlerlogin := login.NewHandler(serviceLogin)
 	// LLamado de las capas para poder maninpular usuarios
 	repo := users.NewRepository(connect)
-	service := users.NewService(repo)
+	service := users.NewService(repo, repoLogin)
 	handler := users.NewHandler(service)
-	// LLamado de las capas para poder manipular equipos de mantenimiento
-	repoTeamOfMaintenance := teammaintenance.NewRepository(connect)
-	serviceTeamOfMaintenance := teammaintenance.NewService(repoTeamOfMaintenance)
-	handlerTeamOfMaintenance := teammaintenance.NewHandler(serviceTeamOfMaintenance)
-	// Llamado capas para poder manipular mantenimientos
-	repoMaintenance := maintenances.NewRepository(connect)
-	serviceMaintenance := maintenances.NewService(repoMaintenance)
-	handlerMaintenance := maintenances.NewHanlder(serviceMaintenance)
 	// Llamado capas para poder manipular clientes
 	repoClients := clients.NewRepository(connect)
-	serviceClients := clients.NewService(repoClients)
+	serviceClients := clients.NewService(repoClients, repoLogin)
 	handlerClients := clients.NewHandler(serviceClients)
+
 	// Creacion de nuevo enrutador
 	r := chi.NewRouter()
 	// URLS respuestas http (usuario)
 	// {id} : va enrutado con el id de chi.URLParam(r, "id")
 	r.Post("/users", handler.HandlerCreateUsers)
-	r.Post("/login", handler.HandlerLogin)
 	r.Get("/users/{id}", handler.HandlerConsultUserInformation)
 	r.Put("/users/{id}", handler.HandlerUpdateUsersInformation)
 	r.Delete("/users/{id}", handler.HandlerDeleteUsers)
 	r.Get("/allUsers", handler.HandlerConsultAllUsers)
 	r.Get("/UsersSearch/{search}", handler.HandlerUsersSearch)
-	// URLS respuestas http (equipos de mantenimientos)
-	r.Post("/TeamOfMaintenance", handlerTeamOfMaintenance.HandlerCreationTeamOfMaintenance)
-	r.Get("/TeamOfMaintenance/{id}", handlerTeamOfMaintenance.HandlerConsultTeamOfMaintenance)
-	r.Put("/TeamOfMaintenance/{id}", handlerTeamOfMaintenance.HandlerUpdateInfoTeamOfMaintenance)
-	r.Delete("/TeaemOfMaintenance/{id}", handlerTeamOfMaintenance.HandlerDeleteTeamOfMaintenance)
-	// URLS respuestas http (mantenimientos)
-	r.Post("/Maintenance", handlerMaintenance.HandlerCreationMaintenance)
-	r.Get("/Maintenance/{id}", handlerMaintenance.HandlerConsultInformationMaintenance)
-	r.Put("/Maintenance/{id}", handlerMaintenance.HandlerUpdateInformationMaintenance)
-	r.Delete("/Maintenance/{id}", handlerMaintenance.HandlerDeleteMaintenance)
 	// URSL respuestas htpp (clientes)
 	r.Post("/AddClients", handlerClients.HandlerCreationClients)
+	r.Get("/client/{id}", handlerClients.HandlerConsultClientInformation)
 	r.Get("/AllClients", handlerClients.HandlerConsultAllClients)
+	r.Put("/client/{id}", handlerClients.HandlerUpdateClients)
+	r.Delete("/client/{id}", handlerClients.HandlerDeleteClient)
+	r.Get("/ClientSearch/{search}", handlerClients.HandlerClientsSearch)
+	// URLS respuestas http (Login)
+	r.Post("/login", handlerlogin.HandlerLogin)
 	// Permite establecer la ruta html del index
 	r.Get("/index", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./web/templates/index.html")
@@ -70,14 +61,19 @@ func main() {
 	r.Get("/createUsers", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./web/templates/admin/forms/users/add_users.html")
 	})
+	// Permite establecer la ruta html de crear empresa
+	r.Get("/CreateCompany", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/templates/users/company.html")
+	})
 	// Permite establecer la ruta html del login
-	r.Get("/loginUsers", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./web/templates/users/login.html")
+	r.Get("/login", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/templates/login/login.html")
 	})
 	// Permite establecer la ruta html de pagina de informacion de usuarios
 	r.Get("/usersInfo", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./web/templates/users/users.html")
 	})
+	// Permite ingresar al formulario de actualizacion de datos usuario
 	r.Get("/FormUpdateUsers", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./web/templates/admin/forms/users/updateUsers.html")
 	})
@@ -85,13 +81,13 @@ func main() {
 	r.Get("/adminHome", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./web/templates/pages/adminHome.html")
 	})
-	// Pagina inicial para el supervisor
-	r.Get("/supervisorHome", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./web/templates/pages/supervisorHome.html")
+	// Pagina inicial para el usaurios
+	r.Get("/userHome", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/templates/pages/userHome.html")
 	})
-	// Pagina inicial para el tecnico
-	r.Get("/TechnicalHome", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./web/templates/pages/technicalHome.html")
+	// Pagina inicial para el clientes
+	r.Get("/clientHome", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/templates/pages/clientHome.html")
 	})
 	// pagina navegador general
 	r.Get("/nav", func(w http.ResponseWriter, r *http.Request) {
@@ -108,6 +104,10 @@ func main() {
 	// Pagina agregar cliente
 	r.Get("/addClient", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./web/templates/admin/forms/clients/add_client.html")
+	})
+
+	r.Get("/FormUpdateClients", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/templates/admin/forms/clients/updateClient.html")
 	})
 	// Ruta para poder trabajar con los archivos statics como css, js, etc...
 	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./web/static/"))))
