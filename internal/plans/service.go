ea4c1947c8261203fe34b0846cfa584aca44c501
Cristian Molina <cm1094871@gmail.com>
2026-08-19 20:43:18 -0500

    servicios con logica interna para funciones

--- internal/plans/service.go
+++ internal/plans/service.go
@@ -2,7 +2,7 @@ package plans
 
 import (
 	"errors"
-	"time"
+	"strings"
 )
 
 // Recibe la conexion real db de Repository
@@ -18,10 +18,13 @@ func NewService(r *RepositoryPlans) *ServicePlans {
 // Variables que guarda el error de existencia de plan
 var ErrUserAlreadyaPlans = errors.New("plan ya existe en el sistema")
 
-// Creacion de usuario y requirimientos a seguir
-func (s *ServicePlans) ServiceCreatetUser(name, description string, price, durationMonths, maxUser int, expirationDate time.Time) error {
+// Creacion de plan y requirimientos a seguir
+func (s *ServicePlans) ServiceCreatetPlans(name, description string, price, monthsduration, userMax int) error {
+	// Se pasa el nombre siempre a minuscula y evita duplicados
+	// Se manejan los espacios y evita duplicado por espacios entre palabras
+	LowerCaseName := strings.ToLower(strings.Join(strings.Fields(name), " "))
 	// Validacion para saber si plan ya esta en sistema y evitar creacion
-	ExistPlan, err := s.repo.QueryPlanExistOfName(name)
+	ExistPlan, err := s.repo.QueryPlanExistOfName(LowerCaseName)
 	if err != nil {
 		return err
 	}
@@ -29,8 +32,8 @@ func (s *ServicePlans) ServiceCreatetUser(name, description string, price, durat
 		return ErrUserAlreadyaPlans
 	}
 	// Validaion de se pemiten campos vacios
-	if name == "" || description == "" || price == 0 || durationMonths == 0 || maxUser == 0 || expirationDate.IsZero() {
+	if LowerCaseName == "" || description == "" || price == 0 || monthsduration == 0 || userMax == 0 {
 		return errors.New("todos los campos son obligatorios")
 	}
-	return s.repo.QueryInsertPlans(name, description, price, durationMonths, maxUser, expirationDate)
+	return s.repo.QueryInsertPlans(LowerCaseName, description, price, monthsduration, userMax)
 }
