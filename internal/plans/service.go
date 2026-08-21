8271f82a21e658743d4226148e7f8840c7a1e575
Cristian Molina <cm1094871@gmail.com>
2026-08-20 19:55:05 -0500

    servicios con logica interna de funciones

--- internal/plans/service.go
+++ internal/plans/service.go
@@ -2,6 +2,7 @@ package plans
 
 import (
 	"errors"
+	"modulo/internal/models"
 	"strings"
 )
 
@@ -19,7 +20,7 @@ func NewService(r *RepositoryPlans) *ServicePlans {
 var ErrUserAlreadyaPlans = errors.New("plan ya existe en el sistema")
 
 // Creacion de plan y requirimientos a seguir
-func (s *ServicePlans) ServiceCreatetPlans(name, description string, price, monthsduration, userMax int) error {
+func (s *ServicePlans) ServiceCreatetPlans(name, description string, userMax int) error {
 	// Se pasa el nombre siempre a minuscula y evita duplicados
 	// Se manejan los espacios y evita duplicado por espacios entre palabras
 	LowerCaseName := strings.ToLower(strings.Join(strings.Fields(name), " "))
@@ -32,8 +33,13 @@ func (s *ServicePlans) ServiceCreatetPlans(name, description string, price, mont
 		return ErrUserAlreadyaPlans
 	}
 	// Validaion de se pemiten campos vacios
-	if LowerCaseName == "" || description == "" || price == 0 || monthsduration == 0 || userMax == 0 {
+	if LowerCaseName == "" || description == "" || userMax == 0 {
 		return errors.New("todos los campos son obligatorios")
 	}
-	return s.repo.QueryInsertPlans(LowerCaseName, description, price, monthsduration, userMax)
+	return s.repo.QueryInsertPlans(LowerCaseName, description, userMax)
+}
+
+// Servicio para llamar todos los planes
+func (s *ServicePlans) ServiceAllPlans() ([]models.PlansAll, error) {
+	return s.repo.QueryAllPlans()
 }
