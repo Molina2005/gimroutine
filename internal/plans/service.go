6c1f7c526c3674137943e737c9554a35bce0be99
Cristian Molina <cm1094871@gmail.com>
2026-08-22 20:07:16 -0500

    servicio con logica interna de cada una de las funcionalidades

--- internal/plans/service.go
+++ internal/plans/service.go
@@ -20,7 +20,7 @@ func NewService(r *RepositoryPlans) *ServicePlans {
 var ErrUserAlreadyaPlans = errors.New("plan ya existe en el sistema")
 
 // Creacion de plan y requirimientos a seguir
-func (s *ServicePlans) ServiceCreatetPlans(name, description string, userMax int) error {
+func (s *ServicePlans) ServiceCreatetPlans(name, description string, userMax, Months, Price int) error {
 	// Se pasa el nombre siempre a minuscula y evita duplicados
 	// Se manejan los espacios y evita duplicado por espacios entre palabras
 	LowerCaseName := strings.ToLower(strings.Join(strings.Fields(name), " "))
@@ -33,10 +33,14 @@ func (s *ServicePlans) ServiceCreatetPlans(name, description string, userMax int
 		return ErrUserAlreadyaPlans
 	}
 	// Validaion de se pemiten campos vacios
-	if LowerCaseName == "" || description == "" || userMax == 0 {
+	if LowerCaseName == "" || description == "" || userMax == 0 || Months == 0 || Price == 0 {
 		return errors.New("todos los campos son obligatorios")
 	}
-	return s.repo.QueryInsertPlans(LowerCaseName, description, userMax)
+	// Recibe datos basicos de plan y pasa a consulta db para poder retornar el id ya creado
+	// y pasarlo a QueryInsertPrecio
+	Id, err := s.repo.QueryInsertPlans(LowerCaseName, description, userMax)
+	// Se pasa el id del plan creado y se crea su regitro con meses y precio en tabla
+	return s.repo.QueryInsertPrecio(Id, Months, Price)
 }
 
 // Servicio para llamar todos los planes
