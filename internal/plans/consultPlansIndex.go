08eae79da33eb295119632d043a1582aa78cc309
Cristian Molina <cm1094871@gmail.com>
2026-08-27 20:34:35 -0500

    handler que recibe solo los planes que tengan como meses 1

---
+++ internal/plans/consultPlansIndex.go
@@ -0,0 +1,24 @@
+package plans
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (h *HandlerPlans) HandlerPlansIndex(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method != http.MethodGet {
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "metodo_no_permitido",
+		})
+		return
+	}
+	Plans, err := h.S.ServicePlansIndex()
+	if err != nil {
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+	json.NewEncoder(w).Encode(Plans)
+}
