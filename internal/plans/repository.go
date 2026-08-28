10f8f886bd7033c1ec6e4c158197f8bad614898c
Cristian Molina <cm1094871@gmail.com>
2026-08-27 20:30:23 -0500

    consultas a la base de datos

--- internal/plans/repository.go
+++ internal/plans/repository.go
@@ -45,7 +45,7 @@ func (r *RepositoryPlans) QueryInsertPlans(name, description string, userMax int
 
 // Funcion creacion de plan, datos basicos de los precios para manejar [mensual, anual, etc]
 // Insercion de planes datos basicos del plan
-func (r *RepositoryPlans) QueryInsertPrecio(id_plan, Months, Price int) error {
+func (r *RepositoryPlans) QueryInsertPrice(id_plan, Months, Price int) error {
 	ctx := context.Background()
 	query := `INSERT INTO precios (id_plan, meses, precio)
 				VALUES ($1, $2, $3)`
@@ -57,19 +57,19 @@ func (r *RepositoryPlans) QueryInsertPrecio(id_plan, Months, Price int) error {
 }
 
 // Consultar para llamar todos los planes creados
-func (r *RepositoryPlans) QueryAllPlans() ([]models.PlansAll, error) {
+func (r *RepositoryPlans) QueryAllPlans() ([]models.JoinPlanAndPrice, error) {
 	ctx := context.Background()
-	query := `SELECT id_plan, nombre, descripcion, max_usuarios, fecha_creacion
-	FROM planes ORDER BY id_plan ASC`
+	query := `SELECT pns.nombre, pns.descripcion, pns.max_usuarios, pr.meses, pr.precio, pns.fecha_creacion FROM planes AS pns 
+		JOIN precios AS pr ON pr.id_plan = pns.id_plan`
 	data, err := r.db.Query(ctx, query)
 	if err != nil {
 		return nil, err
 	}
 	defer data.Close()
-	var DataPlans []models.PlansAll
+	var DataPlans []models.JoinPlanAndPrice
 	for data.Next() {
-		var plans models.PlansAll
-		if err := data.Scan(&plans.Id, &plans.Name, &plans.Description, &plans.UserMax, &plans.CretionDate); err != nil {
+		var plans models.JoinPlanAndPrice
+		if err := data.Scan(&plans.Name, &plans.Description, &plans.UserMax, &plans.Months, &plans.Price, &plans.CretionDate); err != nil {
 			return nil, err
 		}
 		DataPlans = append(DataPlans, plans)
@@ -77,6 +77,22 @@ func (r *RepositoryPlans) QueryAllPlans() ([]models.PlansAll, error) {
 	return DataPlans, nil
 }
 
-func (r *RepositoryPlans) QueryJoinInformationPlans() ([]models.PlansAll, error) {
-
+// Funcion para unir tablas de planes y precios de planes segun meses
+func (r *RepositoryPlans) QueryPlansIndex() ([]models.JoinPlanAndPrice, error) {
+	ctx := context.Background()
+	query := `SELECT pns.nombre, pns.descripcion, pns.max_usuarios, pr.meses, pr.precio FROM planes AS pns
+		JOIN precios AS pr ON pr.id_plan = pns.id_plan WHERE pr.meses = 1`
+	data, err := r.db.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	var dataPlansAndPrice []models.JoinPlanAndPrice
+	for data.Next() {
+		var plansAndPrice models.JoinPlanAndPrice
+		if err := data.Scan(&plansAndPrice.Name, &plansAndPrice.Description, &plansAndPrice.UserMax, &plansAndPrice.Months, &plansAndPrice.Price); err != nil {
+			return nil, err
+		}
+		dataPlansAndPrice = append(dataPlansAndPrice, plansAndPrice)
+	}
+	return dataPlansAndPrice, nil
 }
